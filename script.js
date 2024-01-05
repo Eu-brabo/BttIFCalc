03e3f24d9ef11e346d0b866b73200a3fadd7db7e
Eu-brabo <83727941+Eu-brabo@users.noreply.github.com>
2024-01-05 04:40:19 -0300

    Update script.js
    Adição de features como o cálculo de médias restantes nescessárias para passar direto, e melhora na interface fazendo dados irrelevantes referentes a cálculos passados serem apagados

--- script.js
+++ script.js
@@ -11,15 +11,33 @@ function submit() {
     var cb = c * 3;
     var db = d * 3;
 
+    var medtresbi = (600 - ab)/8;
+    var meddoisbi = (600 - (ab+bb))/6;
+    var medumbi = (600 - (ab+bb+cb))/3;
     var results = ab + bb + cb + db;
     var finals = (600 - results) / 3;
 
     var a = 1;
-    document.getElementById("grade").innerHTML = results;
-    if (results < 600) {
+    document.getElementById("grade").innerHTML = results + " pontos";
+    if (b + c + d == 0) {
+        document.getElementById("medness").innerHTML = medtresbi + " nas médias seguintes";
+    } else if (c + d == 0) {
+        document.getElementById("medness").innerHTML = meddoisbi + " nas médias seguintes";
+    } else if (d == 0) {
+        document.getElementById("medness").innerHTML = medumbi + " nas médias seguintes";
+        document.getElementById("bool").innerHTML = "";
+        document.getElementById("prv_fin").innerHTML = "";
+    } else if (d != 0) {
+        document.getElementById("medness").innerHTML = "";
+        if (results < 600) {
         document.getElementById("bool").innerHTML = "prova final";
-        document.getElementById("prv_fin").innerHTML = finals
-    } else {
+        document.getElementById("prv_fin").innerHTML = finals + " ponto(s) para passar";
+        } else if (results >= 600) {
         document.getElementById("bool").innerHTML = "passou :D";
+        document.getElementById("prv_fin").innerHTML = "";
     }
+    }
+    
+
+    
 }
